9b9d4480d6b2e970a22bd55777529200932b7508
hemightdivideus <hemightdivideus@gmail.com>
2017-02-07 13:46:11 -0700

    Update videoplayer.js

--- videoplayer.js
+++ videoplayer.js
@@ -1,7 +1,7 @@
 var number = 1
+var video = document.getElementById('myVideo');
 function nextVideo() {
 number = number + 1;
-var video = document.getElementById('#myVideo');
 video.src = "video"+number.toString()+".mp4";
 video.load();
 }
